150f333a8454e27efbab2871a6aa78fc5b34f9b9
Geeoon Chung <geeoon13@gmail.com>
2020-04-17 15:01:03 -0500

    Added scene class.

--- SFMLBoids/src/classes/Boid.h
+++ SFMLBoids/src/classes/Boid.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+//aggregation?
+
 class Boid
 {
+public:
+	Boid();
+	void update();
+private:
+	float x;
+	float y;
+	const float magnitude;
+	float direction; //degrees, where 0 degrees is pointing to the left
 };
 
