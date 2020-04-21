d4f42eb8acaf87ac3c7959aac690b5638e2fe2f0
Geeoon Chung <geeoon13@gmail.com>
2020-04-21 09:36:21 -0500

    Boid points and moves in the right direction

--- SFMLBoids/src/classes/Boid.cpp
+++ SFMLBoids/src/classes/Boid.cpp
@@ -1,23 +1,24 @@
 #include "Boid.h"
 
 Boid::Boid() {
-	x = 0;
-	y = 0;
-	velocity.setDirection(90);
+	x = 300;
+	y = 300;
+	velocity.setDirection(45);
 	velocity.setMagnitude(0.1f);
 	triangle.setPointCount(3); //triangle
-	triangle.setRadius(10);
+	triangle.setRadius(5);
 	triangle.setFillColor(sf::Color::Black);
 	triangle.setOrigin(triangle.getRadius(), triangle.getRadius());
 	triangle.setOutlineColor(sf::Color(0, 255, 65));
-	triangle.setOutlineThickness(3);
+	triangle.setOutlineThickness(2);
 }
 
 void Boid::update() {
 	x += velocity.getXComponent();
-	y += velocity.getYComponent();
+	y -= velocity.getYComponent();
 	std::cout << velocity.getYComponent() << std::endl;
 	triangle.setPosition(x, y);
+	triangle.setRotation(velocity.getDirectionDeg());
 }
 
 sf::CircleShape Boid::getSprite() {
